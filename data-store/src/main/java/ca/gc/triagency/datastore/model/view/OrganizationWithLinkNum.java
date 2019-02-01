7cca8b766743d1eedc98c69dd6e0124e7865e6a7
Mike <michael.a.larochelle@gmail.com>
2019-02-01 13:46:08 -0500

    Changed some column names to match file ( location fields) and added Menu items for Vizualizations as well as moved all vizalizations to secure Tableau Server and Cloud database

--- data-store/src/main/java/ca/gc/triagency/datastore/model/view/OrganizationWithLinkNum.java
+++ data-store/src/main/java/ca/gc/triagency/datastore/model/view/OrganizationWithLinkNum.java
@@ -13,7 +13,7 @@ import ca.gc.triagency.datastore.model.util.LocalizedParametersModel;
 //CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `data_cabin`.`orgs_with_external_link_num` AS select `data_cabin`.`org`.`id` AS `id`,`data_cabin`.`org`.`create_date_time` AS `create_date_time`,`data_cabin`.`org`.`name_en` AS `name_en`,`data_cabin`.`org`.`name_fr` AS `name_fr`,count(`data_cabin`.`org`.`id`) AS `link_num` from (`data_cabin`.`organization` `org` join `data_cabin`.`entity_link_organization` `link` on((`data_cabin`.`org`.`id` = `data_cabin`.`link`.`org_id`))) group by `data_cabin`.`org`.`id`;
 
 @Entity
-@Table(name = "orgs_with_external_link_num", schema = "data_cabin")
+@Table(name = "orgs_with_external_link_num", schema = "data_store")
 public class OrganizationWithLinkNum implements LocalizedParametersModel {
 	@Id
 	private Long id;

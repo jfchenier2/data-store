548ebe4d9a55ededd0bb8659364c0981c595292e
dev_jfs <dev_jfs@C16573.nserc.ca>
2019-02-08 15:18:49 -0500

    switched to basic auth (I want both to work together, not working as expected)

--- data-store/src/main/java/ca/gc/triagency/datastore/app/config/WebSecurityConfig.java
+++ data-store/src/main/java/ca/gc/triagency/datastore/app/config/WebSecurityConfig.java
@@ -1,18 +1,23 @@
 package ca.gc.triagency.datastore.app.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter  {
 //	@Value("${ldap.urls}")
 //	private String ldapUrls;
 //
@@ -28,20 +33,67 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //	@Value("${ldap.user.dn.pattern}")
 //	private String ldapUserDnPattern;
 
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**", "/css/**", "/images/**", "/js/**").permitAll()
+//				.antMatchers("/datasets/**").hasRole("ADMIN").antMatchers("/entities/**", "/api/**", "/reports/**")
+//				.hasRole("AGENCY_USER").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+//				.and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/exception/forbiden-by-role");
+//	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**", "/css/**", "/images/**", "/js/**").permitAll()
 				.antMatchers("/datasets/**").hasRole("ADMIN").antMatchers("/entities/**", "/api/**", "/reports/**")
-				.hasRole("AGENCY_USER").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
-				.and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/exception/forbiden-by-role");
+				.hasRole("AGENCY_USER").anyRequest().authenticated().and().httpBasic();
+
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
+		.url("ldap://localhost:8389/dc=nserc_poc,dc=net").and().passwordCompare()
+		.passwordAttribute("userPassword");
 	}
 
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
-				.url("ldap://localhost:8389/dc=nserc_poc,dc=net").and().passwordCompare()
-				.passwordAttribute("userPassword");
-	}
+//
+//	@Configuration
+//    @Order(1)
+//    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    		auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
+//			.url("ldap://localhost:8389/dc=nserc_poc,dc=net").and().passwordCompare()
+//			.passwordAttribute("userPassword");
+//        }
+//
+//		@Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.authorizeRequests()
+//                .antMatchers("/api/**").hasRole("AGENCY_USER")
+//                .and()
+//                .httpBasic();
+//        }
+//    }
+//	
+//	@Configuration   
+//	@Order(2)
+//	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+//		@Override
+//		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//			auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
+//					.url("ldap://localhost:8389/dc=nserc_poc,dc=net").and().passwordCompare()
+//					.passwordAttribute("userPassword");
+//		}
+//		
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.authorizeRequests().antMatchers("/", "/home", "/webjars/**", "/css/**", "/images/**", "/js/**").permitAll()
+//					.antMatchers("/datasets/**").hasRole("ADMIN").antMatchers("/entities/**", "/reports/**")
+//					.hasRole("AGENCY_USER").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+//					.and().logout(	).permitAll().and().exceptionHandling().accessDeniedPage("/exception/forbiden-by-role");
+//		}
+//	}
+//	
 
 	// @Override
 	// public void configure(AuthenticationManagerBuilder auth) throws Exception
@@ -76,30 +128,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	// return retval;
 	// }
 
-	@Bean
-	LdapTemplate getLdapTemplate(ContextSource contextSource) {
-		LdapTemplate retval = new LdapTemplate(contextSource);
-		retval.setIgnorePartialResultException(true);
-		return retval;
-	}
+//	@Bean
+//	LdapTemplate getLdapTemplate(ContextSource contextSource) {
+//		LdapTemplate retval = new LdapTemplate(contextSource);
+//		retval.setIgnorePartialResultException(true);
+//		return retval;
+//	}
 
 	//
-	// @Bean
-	// @Override
-	// public UserDetailsService userDetailsService() {
-	// Collection<UserDetails> users = new ArrayList<UserDetails>();
-	// users.add(User.withDefaultPasswordEncoder().username("sshrc-user").password("password")
-	// .roles("AGENCY_USER", "SSHRC").build());
-	// users.add(User.withDefaultPasswordEncoder().username("nserc-user").password("password")
-	// .roles("AGENCY_USER", "NSERC").build());
-	// users.add(User.withDefaultPasswordEncoder().username("sshrc-user-edi").password("password")
-	// .roles("AGENCY_USER", "SSHRC", "EDI_AUTHORIZED").build());
-	// users.add(User.withDefaultPasswordEncoder().username("nserc-user-edi").password("password")
-	// .roles("AGENCY_USER", "NSERC", "EDI_AUTHORIZED").build());
-	// users.add(User.withDefaultPasswordEncoder().username("admin").password("password").roles("ADMIN",
-	// "AGENCY_USER")
-	// .build());
-	//
-	// return new InMemoryUserDetailsManager(users);
-	// }
+//	 @Bean
+//	 @Override
+//	 public UserDetailsService userDetailsService() {
+//		 Collection<UserDetails> users = new ArrayList<UserDetails>();
+//		 users.add(User.withDefaultPasswordEncoder().username("sshrc-user").password("password")
+//		 .roles("AGENCY_USER", "SSHRC").build());
+//		 users.add(User.withDefaultPasswordEncoder().username("nserc-user").password("password")
+//		 .roles("AGENCY_USER", "NSERC").build());
+//		 users.add(User.withDefaultPasswordEncoder().username("sshrc-user-edi").password("password")
+//		 .roles("AGENCY_USER", "SSHRC", "EDI_AUTHORIZED").build());
+//		 users.add(User.withDefaultPasswordEncoder().username("nserc-user-edi").password("password")
+//		 .roles("AGENCY_USER", "NSERC", "EDI_AUTHORIZED").build());
+//		 users.add(User.withDefaultPasswordEncoder().username("admin").password("password").roles("ADMIN",
+//		 "AGENCY_USER")
+//		 .build());
+//		
+//		 return new InMemoryUserDetailsManager(users);
+//	 }
 }
